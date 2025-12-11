742dbdefab030e7f233071f84988fb0946e93be7
rafaelbeltz02 <rafaelbeltz947@example.com>
2025-12-11 23:20:42 +0800

    Create Header.jsx

---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header style={{
+      background:"#1e3a57", color:"white",
+      padding:"15px", textAlign:"center", fontSize:"22px"
+    }}>
+      <strong>My BioNote App</strong>
+    </header>
+  );
+}
