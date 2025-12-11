1ad9cef4580c1bb61fd43809e4d9c2babc13dad1
rafaelbeltz02 <rafaelbeltz947@example.com>
2025-12-11 23:37:14 +0800

    Update Footer.jsx

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer style={{
-      background:"#1e3a57", color:"white",
+      background:"#800000", color:"white",
       padding:"10px", textAlign:"center", marginTop:40
     }}>
       © 2025 BioNote — Created using React + Material UI
