ba7292250466d73d5605a255af1a97763377060e
rafaelbeltz02 <rafaelbeltz947@example.com>
2025-12-11 23:37:17 +0800

    Update Header.jsx

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header style={{
-      background:"#1e3a57", color:"white",
+      background:"#800000", color:"white",
       padding:"15px", textAlign:"center", fontSize:"22px"
     }}>
       <strong>My BioNote App</strong>
